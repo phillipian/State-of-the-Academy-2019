db646682aa163c6d10bf08b0fb1d8fd6afac5574
createandbuild <33373263+createandbuild@users.noreply.github.com>
2019-04-18 23:00:23 -0400

    graph for class

--- dist/JS/components/general.js
+++ dist/JS/components/general.js
@@ -1,18 +1,13 @@
-// set the dimensions and margins of the graph
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 700 - margin.left - margin.right,
     height = 200 - margin.top - margin.bottom;
 
-// set the ranges
 var x = d3.scaleBand()
           .range([0, width])
           .padding(0.1);
 var y = d3.scaleLinear()
           .range([height, 0]);
 
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
 var svg = d3.select("#general").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -20,20 +15,15 @@ var svg = d3.select("#general").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// get the data
 d3.csv("../../assets/data/general/class.csv", function(error, data) {
   if (error) throw error;
-
-  // format the data
   data.forEach(function(d) {
     d.class = +d.class;
   });
 
-  // Scale the range of the data in the domains
   x.domain(data.map(function(d) { return d.number; }));
   y.domain([0, d3.max(data, function(d) { return d.class; })]);
 
-  // append the rectangles for the bar chart
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
@@ -43,12 +33,10 @@ d3.csv("../../assets/data/general/class.csv", function(error, data) {
       .attr("y", function(d) { return y(d.class); })
       .attr("height", y.bandwidth());
 
-  // add the x Axis
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-  // add the y Axis
   svg.append("g")
       .call(d3.axisLeft(y));
 
