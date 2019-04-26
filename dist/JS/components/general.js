fe8485466962a6dc2386b13fa3ac12abfca2dc12
createandbuild <33373263+createandbuild@users.noreply.github.com>
2019-04-25 23:52:37 -0400

    added first graph

--- dist/JS/components/general.js
+++ dist/JS/components/general.js
@@ -1,43 +1,34 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 700 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
-
-var x = d3.scaleBand()
-          .range([0, width])
-          .padding(0.1);
-var y = d3.scaleLinear()
-          .range([height, 0]);
-
-var svg = d3.select("#general").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-d3.csv("../../../assets/data/general/class.csv", function(error, data) {
-  if (error) throw error;
-  data.forEach(function(d) {
-    d.class = +d.class;
-  });
-
-  x.domain(data.map(function(d) { return d.number; }));
-  y.domain([0, d3.max(data, function(d) { return d.class; })]);
-
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", 0)
-      .attr("width", function(d) { return x(d.number); })
-      .attr("y", function(d) { return y(d.class); })
-      .attr("height", y.bandwidth());
-
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-  svg.append("g")
-      .call(d3.axisLeft(y));
-
+var chart = Highcharts.chart('class', {
+    chart: {
+        style: {
+            fontFamily: 'sans-serif', fontSize: '15px'
+        }
+    },
+    title: {
+        text: 'What class are you in?'
+    },
+    xAxis: {
+         categories: ['Junior', 'Lower', 'Upper', 'Senior'],
+         title: {
+            text: 'Class'
+         }
+    },
+    yAxis: {
+      min: 0,
+         title: {
+            text: 'Responses',
+            verticalAlign: 'center'
+         },
+         labels: {
+            overflow: 'justify'
+         }
+    },
+    series: [{
+        data: [214, 275, 271, 286],
+        type: 'bar',
+        dataLabels: {
+            enabled: true
+        },
+        name: 'Classes'
+    }]
 });
