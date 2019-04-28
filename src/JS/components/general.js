4cc660fb43affbbf79fa591bb3fd84a22ef80a9a
createandbuild <33373263+createandbuild@users.noreply.github.com>
2019-04-28 14:03:46 -0400

    added financial aid graph

--- src/JS/components/general.js
+++ src/JS/components/general.js
@@ -45,7 +45,7 @@ var chart = Highcharts.chart('q-class', {
         dataLabels: {
             enabled: true
         },
-        name: 'Classes'
+        name: 'Responses'
     }]
 });
 
@@ -106,60 +106,157 @@ var chart = Highcharts.chart('q-boarding', {
     }]
 });
 
-d3.selectAll(".map").each(function(){
-  var total = 0,
-      regionNames = ["Discontinguous", "Northeast", "South", "West", "Midwest", "International"];
+// d3.selectAll(".map").each(function(){
+//   var total = 0,
+//       regionNames = ["Discontinguous", "Northeast", "South", "West", "Midwest", "International"];
+//
+//   var accent = d3.color(this.dataset.accent);
+//
+//   var colors = ["#abdde3","#74cee0","#42b6da", "#13659d","#0c3d8b","#182552"];
+//
+//   var responses = this.dataset.responses.split(",").map(function(element, i){
+//     total += parseInt(element);
+//     return {
+//       responses: parseInt(element),
+//       name: regionNames[i]
+//     };
+//   });
+//
+//   var colorSort = responses.slice().sort(function(a, b) {
+//     return a.responses - b.responses;
+//   });
+//
+//   responses = responses.map(function(element, i){
+//     return {
+//       responses: element.responses,
+//       name: element.name,
+//       color: colors[colorSort.indexOf(element)]
+//     };
+//   });
+//
+//   var currentElement = this,
+//       tooltipText,
+//       tooltip = d3.select(this.firstChild);
+//
+//   d3.svg(this.dataset.url).then(function(res){
+//     var svg = res.documentElement;
+//     d3.select(currentElement).node().appendChild(svg);
+//     d3.select(svg).selectAll("*")
+//       .data(responses)
+//       .style("fill", function(d, i){
+//         return d3.rgb(d.color);
+//       })
+//       .on("mouseover", function(d, i){
+//         d3.select(this).style("fill", d3.rgb(accent.brighter(0.3)));
+//         tooltipText = generateTooltip({title: d.name, responses: d.responses, percentage: d.responses / total});
+//         tooltip.classed("hidden", false).html(tooltipText);
+//       })
+//       .on("mousemove", function(d){
+//         var mouse = d3.mouse(currentElement);
+//
+//         tooltip.style("left", mouse[0] - Math.round(tooltip.node().offsetWidth / 2) + "px")
+//           .style("top", mouse[1] - Math.round(tooltip.node().offsetHeight) - 10 + "px");
+//       })
+//       .on("mouseout", function(d){
+//         d3.select(this).style("fill", d3.rgb(d.color));
+//         tooltip.classed("hidden", true);
+//       });
+//   });
+// });
 
-  var accent = d3.color(this.dataset.accent);
+var chart = Highcharts.chart('q-ethnicity', {
+    chart: {
+        style: {
+            fontFamily: 'Gotham', fontSize: '15px'
+        },
+        backgroundColor: "#ECF0F1"
+    },
+    title: {
+        text: 'What is your ethonicity?',
+        style: {
+          fontWeight: 'bold'
+        }
+    },
+    subtitle: {
+        text: '<i>Note: respondents may have selected more than one</i>',
+    },
+    colors: ['#1f3a93'],
+    xAxis: {
+         categories: ['African', 'African American', 'Asian American', 'East Asian', 'Ethnically Jewish', 'Hispanic', 'Latinx', 'Middle Eastern', 'Native American', 'Pacific Islander', 'South Asian', 'Southeast Asian', 'West Indian/Caribbean', 'White', 'White Hispanic', 'Other']
+    },
+    yAxis: {
+      min: 0,
+      title: {
+          text: 'Responses',
+          verticalAlign: 'center',
+          style: {
+            color: 'black',
+            fontWeight: 'bold'
+          }
+      },
+      labels: {
+        overflow: 'justify'
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    series: [{
+        data: [25, 67, 292, 143, 58, 53, 56, 33, 14, 6, 53, 53, 20, 551, 34, 12],
+        type: 'bar',
+        dataLabels: {
+            enabled: true
+        },
+        name: 'Responses'
+    }]
+});
 
-  var colors = ["#abdde3","#74cee0","#42b6da", "#13659d","#0c3d8b","#182552"];
-
-  var responses = this.dataset.responses.split(",").map(function(element, i){
-    total += parseInt(element);
-    return {
-      responses: parseInt(element),
-      name: regionNames[i]
-    };
-  });
-
-  var colorSort = responses.slice().sort(function(a, b) {
-    return a.responses - b.responses;
-  });
-
-  responses = responses.map(function(element, i){
-    return {
-      responses: element.responses,
-      name: element.name,
-      color: colors[colorSort.indexOf(element)]
-    };
-  });
-
-  var currentElement = this,
-      tooltipText,
-      tooltip = d3.select(this.firstChild);
-
-  d3.svg(this.dataset.url).then(function(res){
-    var svg = res.documentElement;
-    d3.select(currentElement).node().appendChild(svg);
-    d3.select(svg).selectAll("*")
-      .data(responses)
-      .style("fill", function(d, i){
-        return d3.rgb(d.color);
-      })
-      .on("mouseover", function(d, i){
-        d3.select(this).style("fill", d3.rgb(accent.brighter(0.3)));
-        tooltipText = generateTooltip({title: d.name, responses: d.responses, percentage: d.responses / total});
-        tooltip.classed("hidden", false).html(tooltipText);
-      })
-      .on("mousemove", function(d){
-        var mouse = d3.mouse(currentElement);
-
-        tooltip.style("left", mouse[0] - Math.round(tooltip.node().offsetWidth / 2) + "px")
-          .style("top", mouse[1] - Math.round(tooltip.node().offsetHeight) - 10 + "px");
-      })
-      .on("mouseout", function(d){
-        d3.select(this).style("fill", d3.rgb(d.color));
-        tooltip.classed("hidden", true);
-      });
-  });
+var chart = Highcharts.chart('q-financial', {
+    chart: {
+        type: 'pie',
+        style: {
+            fontFamily: 'Gotham', fontSize: '15px'
+        },
+        backgroundColor: "#ECF0F1"
+    },
+    title: {
+        text: 'Are you on any form of financial aid?',
+        style: {
+          fontWeight: 'bold'
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.y}</b>'
+    },
+    plotOptions: {
+        pie: {
+            colors: pieColors,
+            borderWidth: 0,
+            borderColors: null,
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: 'black',
+                    fontWeight: 'bold'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Responses',
+        colorByPoint: true,
+        data: [{
+            name: 'Yes, from Andover',
+            y: 483,
+        },
+        {
+            name: 'Yes, from outside of Andover',
+            y: 50,
+        },
+        {
+            name: 'No',
+            y: 536
+        }]
+    }]
 });
