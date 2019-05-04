68aec508eb72825f2d9eab5a92b000174f631667
createandbuild <33373263+createandbuild@users.noreply.github.com>
2019-05-04 19:31:11 -0400

    fixed tabs

--- src/JS/script.js
+++ src/JS/script.js
@@ -7,19 +7,26 @@ $(window).scroll(function () {
   }
 });
 
-function openGraph(evt, tabName) {  //function overrides - DOM with the div - nthChild
+function openGraph(evt, tabName, id) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  parent = document.getElementById(id);
+  children = parent.childNodes;
+  for(var i = 0; i < children.length; i++){
+    if(children[i].className && children[i].className.indexOf("tabcontent") != -1) children[i].style.display = "none";
+    else if(children[i].className == "tab"){
+      var tabs = children[i].childNodes;
+      for(var j = 0; j < tabs.length; j++){
+        if(tabs[j].className && tabs[j].className.indexOf("tablinks") != -1) tabs[j].className = tabs[j].className.replace(" active", "")
+
+      }
+    }
   }
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
-// document.getElementById("defaultOpen2").click();
+var openDefault = document.getElementsByClassName("defaultOpen");
+
+for(var i=0; i < openDefault.length; i++) {
+  openDefault[i].click();
+}
